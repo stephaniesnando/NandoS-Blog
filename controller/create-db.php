6bbc209d02f28003622438cbb17472831251cd90
Stephanie Nando <stephaniesnando@gmail.com>
2015-01-28 11:26:29 -0800

    day 2

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,8 +1,17 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once(__DIR__ . "/../model/database.php");
 
+$connection = new mysqli($host, $username, $password);
+
+if($connection->connect_error){
+    die("Error: " . $conncection->Connect_error);
+}
+
+$sexists = $connection->select_db($database);
+
+if($exists) {
+   echo "database exists"; 
+}
+
+$connection->close();
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "Web server is running locally";
+            require_once(__DIR__ . "/controller/create-db.php");
         ?>
     </body>
 </html>
