231f61c19c8e9fd42dcb427d0a9ffc9e8ba8ce5c
Stephanie Nando <stephaniesnando@gmail.com>
2015-01-29 11:28:19 -0800

    day 3

--- controller/create-db.php
+++ controller/create-db.php
@@ -8,10 +8,23 @@ if($connection->connect_error){
     die("Error: " . $conncection->Connect_error);
 }
 
-$sexists = $connection->select_db($database);
+$exists = $connection->select_db($database);
 
-if($exists) {
-   echo "database exists"; 
-}
+if(!$exists) {
+  $query = $connection->query("CREATE DATABASE $database");
+  
+  if($query){
+      echo "Succesfully created database: " . $database;
+  }
+  }else{
+      echo "data base already  exists. ";
+  }
+
+  $query = $connection->query("CREATE TABLE posts("
+          . "id int(11) NOT NULL AUTO_INCREMENT,"
+          . "title varchara(255) NOT NULL"
+          . "post text NOT NULL"
+          . "PRIMARY KEY (id))");
+
 
 $connection->close();
