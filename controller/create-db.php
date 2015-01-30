d6d62b252e4d9297fe30dd45871c57d885e01c5c
Stephanie Nando <stephaniesnando@gmail.com>
2015-01-30 11:24:00 -0800

    Day 4

--- controller/create-db.php
+++ controller/create-db.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . "/../model/database.php");
 $connection = new mysqli($host, $username, $password);
 
 if($connection->connect_error){
-    die("Error: " . $conncection->Connect_error);
+    die("<p>Error: " . $conncection->Connect_error . "</p>");
 }
 
 $exists = $connection->select_db($database);
@@ -14,17 +14,24 @@ if(!$exists) {
   $query = $connection->query("CREATE DATABASE $database");
   
   if($query){
-      echo "Succesfully created database: " . $database;
+      echo "<p>Succesfully created database: " . $database . "</p>";
   }
   }else{
-      echo "data base already  exists. ";
+      echo "<p>Data base already  exists. </p>";
   }
 
   $query = $connection->query("CREATE TABLE posts("
           . "id int(11) NOT NULL AUTO_INCREMENT,"
-          . "title varchara(255) NOT NULL"
-          . "post text NOT NULL"
+          . "title varchar(255) NOT NULL,"
+          . "post text NOT NULL,"
           . "PRIMARY KEY (id))");
+  
+  if($query){
+      echo "<p>Successfully created table: posts</p>";
+  }
+  else{
+      echo "<p>$connection->error</p>";
+  }
 
 
 $connection->close();
---
+++ view/navigation.php
@@ -0,0 +1,6 @@
+<nav>
+    <ul>
+        <li><a href="/..post.php"> Blog Post Form</a> </li>
+    </ul>
+</nav>
+
