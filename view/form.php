a2e083af09e9f5c3b087b8afdd7e17ff351800e0
Stephanie Nando <stephaniesnando@gmail.com>
2015-02-03 11:05:11 -0800

    Day 7

--- view/form.php
+++ view/form.php
@@ -1,6 +1,10 @@
+<?php
+    require_once(__DIR__ . "/../model/config.php");
+?>
+
 <h1> Create Blog Post</h1>
 
-<form>
+<form method="post" action="<?php echo $path . "controller/create-post.php";  ?>">
     <div>
         <label for="title">Title: </label>
         <input type="text" name="title" />
@@ -9,5 +13,9 @@
     <div>
         <label for="post">Post: </label>
         <textarea name="post"></textarea>
+    </div>
+    
+    <div> 
+        <button type="submit">  Submit </button>
     </div>
 </form>
