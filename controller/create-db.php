773e0586a3888265ff90db40a0905aa464860ee4
Stephanie Nando <stephaniesnando@gmail.com>
2015-02-04 11:06:26 -0800

    Day 8, I think

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__ . "/../model/database.php");
+require_once(__DIR__ . "/../model/config.php");
 
 $connection = new mysqli($host, $username, $password);
 
--- model/config.php
+++ model/config.php
@@ -1,3 +1,7 @@
 <?php
-
 $path = "/NandoS-Blog/";
+
+$host = "localhost";
+$username = "root";
+$password = "root";
+$database = "blog_db";
