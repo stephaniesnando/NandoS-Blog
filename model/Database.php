2a5451c57bcc40c411ab8840b925eaaff703973d
Stephanie Nando <stephaniesnando@gmail.com>
2015-02-06 11:23:02 -0800

    Day 9 ish

--- model/Database.php
+++ model/Database.php
@@ -1,8 +1,42 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Database {
 
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+
+    public function __construct($host, $username, $password, $database) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+    }
+
+    public function openConnection() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($this->connection->connect_error) {
+            die("<p>Error: " . $this->conncection->Connect_error . "</p>");
+        }
+    }
+
+    public function closeConnection() {
+        if(isset($this->connection)){
+            $this->connection->close();
+        }
+    }
+
+    public function query($string) {
+        $this->openConnection();
+        
+        $query = $this->connection->query($string);
+        
+        $this->closeConnection();
+        
+        return $query;
+    }
+
+}
