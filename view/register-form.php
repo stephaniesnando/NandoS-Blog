115a455d996c9ae5b697ae0a8e27f876f59f7ebb
Stephanie Nando <stephaniesnando@gmail.com>
2015-02-11 11:24:10 -0800

    Day 11

---
+++ view/register-form.php
@@ -0,0 +1,22 @@
+<h1>Register</h1>
+
+<form> 
+    <div>
+        <label for="email">Email: </label>
+        <input type="text" name="email"/>
+    </div>
+    
+    <div>
+        <label for="username">Username: </label>
+        <input type="text" name="username"/>
+    </div>
+    
+    <div>
+        <label for="password">Password: </label>
+        <input type="password" name="password"/>
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+</form>
