419e1b893a474cb69978d2ca9a0521033a3a70da
Stephanie Nando <stephaniesnando@gmail.com>
2015-02-15 13:37:12 -0800

    Day 12 I think (at home)

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,4 +1,14 @@
 <?php
+require_once(__DIR__ . "/../model/config.php");
 
+$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+echo $password;
+
+$salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
+
+echo $salt;
 
 
--- view/register-form.php
+++ view/register-form.php
@@ -1,6 +1,11 @@
+<?php
+    require_once(__DIR__ . "/../model/config.php");
+?>
+
+
 <h1>Register</h1>
 
-<form> 
+<form method="post" action="<?php echo $path . "controller/create-user.php";  ?>"> 
     <div>
         <label for="email">Email: </label>
         <input type="text" name="email"/>
