7c6b963492c91b4773a99917c970eabe269ab386
Stephanie Nando <stephaniesnando@gmail.com>
2015-02-17 11:20:20 -0800

    Day 13 I think

--- controller/create-user.php
+++ controller/create-user.php
@@ -5,10 +5,20 @@ $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-echo $password;
-
 $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
 
-echo $salt;
 
+$hashedPassword = crypt($password, $salt);
 
+$query = $_SESSION["connection"]->query("INSERT INTO users SET "
+        . "email = '$email',"
+        . "username = '$username',"
+        . "password = '$hashedPassword',"
+        . "salt = '$salt'");
+
+if($query){
+    echo  "Successfully created user: $username";
+}
+else{
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
--- index.php
+++ index.php
@@ -4,5 +4,6 @@
     require_once(__DIR__ . "/view/navigation.php");
     require_once(__DIR__ . "/controller/create-db.php");
     require_once(__DIR__ . "/view/footer.php");
+    require_once(__DIR__ . "/controller/read-posts.php");
 ?>
     
