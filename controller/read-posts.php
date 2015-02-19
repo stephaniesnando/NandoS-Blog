b622f0c585fe4a3630fe672976da3600de0dbf3a
Stephanie Nando <stephaniesnando@gmail.com>
2015-02-19 11:22:44 -0800

    Day 14 maybe

--- controller/read-posts.php
+++ controller/read-posts.php
@@ -6,10 +6,10 @@ $result = $_SESSION["connection"]->query($query);
 
 if($result){
     while($row = mysqli_fetch_array($result)){
-        echo "<div class = 'post>";
-        echo "<h2>" . $row['title'] . "<h2>";
+        echo "<div class = 'post'>";
+        echo "<h2>" . $row['title'] . "</h2>";
         echo "<br />";
-        echo "<p>" . $row['post'] . "</h1>";
+        echo "<p>" . $row['post'] . "</p>";
         echo "</div>";
     }
 }
